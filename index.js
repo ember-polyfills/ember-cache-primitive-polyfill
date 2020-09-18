ae907c6a46e8da808d19cdce983d2d20833aa7e4
Robert Jackson <rjackson@linkedin.com>
2020-09-18 10:06:38 -0400

    Make inert for Ember 3.22.0-alpha.1 and higher.
    This feature is now enabled by default in Ember 3.22, this ensures that
    the polyfill does not run in those versions.

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 'use strict';
 
+const VersionChecker = require('ember-cli-version-checker');
+const NATIVE_SUPPORT_VERSION = '3.22.0-alpha.1';
+let hasBeenWarned = false;
+
 module.exports = {
   name: require('./package').name,
 
   included() {
     this._super.included.apply(this, arguments);
 
-    this.import('vendor/ember-cache-primitive-polyfill.js');
+    const checker = new VersionChecker(this);
+    const emberVersion = checker.for('ember-source');
+
+    if (emberVersion.lt(NATIVE_SUPPORT_VERSION)) {
+      this.import('vendor/ember-cache-primitive-polyfill.js');
+    } else if (this.parent === this.project && !hasBeenWarned) {
+      this.ui.writeWarnLine(
+        `${this.name} is not required for Ember ${NATIVE_SUPPORT_VERSION} and later, please remove from your 'package.json'.`
+      );
+      hasBeenWarned = true;
+    }
   },
 
   treeForVendor(rawVendorTree) {
